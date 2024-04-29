026591ac4c1ab7c6e042bb8cb82e2c659fa01d91
Bart Duisters <bartduisters@bartduisters.com>
2024-04-29 15:49:33 +0200

    refactor: price details without pivot table

--- database/migrations/2024_04_29_115228_create_price_details_table.php
+++ database/migrations/2024_04_29_115228_create_price_details_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('price_details', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('course_id')->constrained()->cascadeOnDelete();
             $table->string('name');
             $table->float('price_excl');
             $table->float('price_incl');
