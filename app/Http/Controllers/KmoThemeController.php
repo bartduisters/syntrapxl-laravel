1a8684ec490439b0f458466143f3dbf2f626c4cc
Bart Duisters <bartduisters@bartduisters.com>
2024-04-29 13:47:39 +0200

    feat: kmo theme migration

---
+++ app/Http/Controllers/KmoThemeController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\KmoTheme;
+use Illuminate\Http\Request;
+
+class KmoThemeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(KmoTheme $kmoTheme)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(KmoTheme $kmoTheme)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, KmoTheme $kmoTheme)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(KmoTheme $kmoTheme)
+    {
+        //
+    }
+}
---
+++ app/Models/KmoTheme.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KmoTheme extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name'];
+}
