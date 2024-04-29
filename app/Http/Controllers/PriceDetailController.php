2ec965c5b845ad50dbb7a363e3a12a11154748bd
Bart Duisters <bartduisters@bartduisters.com>
2024-04-29 13:53:38 +0200

    feat: price detail migration

---
+++ app/Http/Controllers/PriceDetailController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PriceDetail;
+use Illuminate\Http\Request;
+
+class PriceDetailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PriceDetail $priceDetail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PriceDetail $priceDetail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, PriceDetail $priceDetail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PriceDetail $priceDetail)
+    {
+        //
+    }
+}
