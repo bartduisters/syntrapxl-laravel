31352a4bf2057b0675961d291494f074afcd0f87
Bart Duisters <bartduisters@bartduisters.com>
2024-04-29 14:53:10 +0200

    feat: unique constraints on tables

--- database/migrations/2024_04_29_114708_create_kmo_themes_table.php
+++ database/migrations/2024_04_29_114708_create_kmo_themes_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('kmo_themes', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->timestamps();
         });
     }
