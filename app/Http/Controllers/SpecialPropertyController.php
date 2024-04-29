e447c5cf0e5250ffc3f529e2cc3644db67c5d507
Bart Duisters <bartduisters@bartduisters.com>
2024-04-29 13:51:13 +0200

    feat: special property migration

---
+++ app/Http/Controllers/SpecialPropertyController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SpecialProperty;
+use Illuminate\Http\Request;
+
+class SpecialPropertyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SpecialProperty $specialProperty)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SpecialProperty $specialProperty)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, SpecialProperty $specialProperty)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SpecialProperty $specialProperty)
+    {
+        //
+    }
+}
