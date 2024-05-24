02321f93aa2c6a1bd0c2463bf842bfa4bdb10308
Bart Duisters <bartduisters@bartduisters.com>
2024-05-24 21:28:07 +0200

    feat: courses as main page

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\CourseController;
 use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/opleidingen');
 });
 
 Route::get('/dashboard', function () {
